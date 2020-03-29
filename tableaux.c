f948218c3700dc3dd94a41631b51ebbc0b08f29c
Pierrick444 <58504870+Pierrick444@users.noreply.github.com>
2020-03-29 16:11:43 +0200

    Add files via upload

--- tableaux.c
+++ tableaux.c
@@ -20,9 +20,9 @@ int main()
     //printf("%s", tab1[]);
     return 0;
 
-    char* combinaisons[28][10] =
+    char* combinaisons[28][12] =
     {
-        "DEM",
+        "DEM",//1
         "DOM",
         "DEO",
         "DOL",
@@ -48,6 +48,19 @@ int main()
         "II IV VI",
         "IV V VI",
         "III IV V",
+        "I I I",
+        "II II II",
+        "III III III",
+        "IV IV IV",
+        "V V V",
+        "VI VI VI",
+        "M M M",
+        "O O O",
+        "U U U",
+        "D D D",
+        "L L L",
+        "E E E",//38
+
     };
 
     }
