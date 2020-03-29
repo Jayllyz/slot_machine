134f7d5b3b6c0fe73db3fa1bbf6c66e048105207
Pierrick444 <58504870+Pierrick444@users.noreply.github.com>
2020-03-29 16:04:37 +0200

    Add files via upload

---
+++ tableaux.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+    char* tab1[13] =
+    {
+    "M",
+    "O",
+    "U",
+    "D",
+    "L",
+    "E",
+    "I",
+    "II",
+    "III",
+    "IV",
+    "V",
+    "VI",
+    };
+    //printf("%s", tab1[]);
+    return 0;
+
+    char* combinaisons[28][10] =
+    {
+        "DEM",
+        "DOM",
+        "DEO",
+        "DOL",
+        "DUE",
+        "DUO",
+        "EMU",
+        "LED",
+        "LEM",
+        "MEL",
+        "MEO",
+        "MOL",
+        "MOU",
+        "MUE",
+        "ODE",
+        "OUD",
+        "ELU",
+        "LEU",
+        "LUE",
+        "LUO",
+        "OLE",
+        "I II III",
+        "I III V",
+        "II IV VI",
+        "IV V VI",
+        "III IV V",
+    };
+
+    }
+}
