4ed367b5402c165660f9da9f142b4945fc14d4fb
Jayllyz <antonydavid945@gmail.com>
2020-06-01 17:53:53 +0200

    V1

---
+++ fonctions.h
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void cursorPosition(int YPos, int XPos) //Fonction pour déplacer le curseur
+{
+    printf("\033[%d;%dH",YPos,XPos);
+}
+
+int get_mise()
+{
+	char scan[7];
+	char quitter[2]="0";
+	char un[2]="1";
+	char deux[2]="2";
+	char trois[2]="3";
+	cursorPosition(23,61);
+	printf("      ");
+	cursorPosition(23,61);
+	scanf("%s", scan);
+	if(strcmp(scan,quitter)==0)
+	{
+		exit(-1);
+	}
+
+	if(strcmp(scan,un)==0) 
+	{
+		return atoi(scan);
+	}
+
+	if(strcmp(scan,deux)==0) 
+	{
+		return atoi(scan);
+	}
+
+	if(strcmp(scan,trois)==0) 
+	{
+		return atoi(scan);
+	}
+	else
+	{
+		cursorPosition(25,55);
+		printf("Impossible !");
+		get_mise();
+	}
+	return 0;
+}
+
+
+void affichage( int i,int L)
+{
+	int tab_caractere[12] = {0,684,1368,2052,2736,3420,4104,4788,5472,6156,6840,7524};
+	int C=2;
+	FILE* fichier = NULL;
+	int caractere = 0;
+	fichier = fopen("lettres_chiffres_romains.txt", "r"); //on ouvre le fichier en lecture
+	int var = tab_caractere[i];
+
+	fseek(fichier,var,SEEK_SET);
+
+	if(fichier != NULL) //on verifie que le fichier existe bien 
+	{ 
+		for(int y=0; y < 18; y++)
+		{
+			printf("\033[%d;%dH",C,L);
+			for (int j = 0; j < 38; j++)
+			{
+				caractere = fgetc(fichier);
+				printf("%c", caractere);
+			
+			}
+			C++;
+		}
+	} 
+	fclose(fichier); 
+}
+
+char* concatener(const char *a, const char *b, const char *c )
+{
+	char *combi = malloc(1 + 1 +1 +1);
+	if(combi)
+	{
+		memcpy(combi,a,1);
+		memcpy(combi+1,b,1);
+		memcpy(combi + 1 + 1,c,1 +1);
+	}
+	return combi;
+}
--- projet.c
+++ projet.c
@@ -2,198 +2,233 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+#include "fonctions.h"
 
-
-char* elements[13] =
+char* elements[12] =
 {
-    "M",
-    "O",
-    "U",
-    "D",
-    "L",
-    "E",
-    "I",
-    "II",
-    "III",
-    "IV",
-    "V",
-    "VI",
+	"M",
+	"O",
+	"U",
+	"D",
+	"L",
+	"E",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
 };
-    char combinaisons[40][13] =
-    {
-        "DEM",
-        "DOM",
-        "DEO",
-        "DOL",
-        "DUE",
-        "DUO",
-        "EMU",
-        "LED",
-        "LEM",
-        "MEL",
-        "MEO",
-        "MOL",
-        "MOU",
-        "MUE",
-        "ODE",
-        "OUD",
-        "ELU",
-        "LEU",
-        "LUE",
-        "LUO",
-        "OLE",
-        "I II III", //123
-        "III IV V", //345
-        "IV V VI", //456
-        "II III IV", //234
-        "I III V", //135
-        "II IV VI", //246
-        "I I I", //111
-        "II II II", //222
-        "III III III", //333
-        "IV IV IV", //444
-        "V V V", //555
-        "VI VI VI", //666
-        "MMM",
-        "OOO",
-        "UUU",
-        "DDD",
-        "LLL",
-        "EEE",
 
-    };
-
-	int tabgain[40]=
-	{
-		25, //DEM
-		25, //DOM
-		250, //DEO
-		25, //DOL
-		25, //DUE
-		250, //UO
-		25, //EMU
-		1000, //LED
-		25, //LEM
-		25, //MEL
-		25, //MEO
-		250, //MOL
-		500, //MOU
-		25,  //MUE
-		25, //ODE
-		25, //OUD
-		25, //ELU
-		25, //LEU
-		25, //LUE
-		25, //LUO
-		25, //OLE
-		250, //123
-		60, //345
-		250, //456
-		40, //234
-		25, //135
-		50, //246
-		1000, //111
-		200, //222
-		300, //333
-		400, //444
-		5, //555
-		6, //666
-		100, //MMM
-		100, //OOO
-		100, //UUU
-		100, //DDD
-		100, //LLL
-		100, ///EEE
-	};
-
-
-int get_mise()
+char combinaisons[40][4] =
 {
-	char scan[7];
-	char quitter[7]="quitter";
-	char un[2]="1";
-	char deux[2]="2";
-	char trois[2]="3";
-	scanf("%s", scan);
-	if(strcmp(scan,quitter)==0)
-	{
-		exit(-1);
-	}
+	"DEM",
+	"DOM",
+	"DEO",
+	"DOL",
+	"DUE",
+	"DUO",
+	"EMU",
+	"LED",
+	"LEM",
+	"MEL",
+	"MEO",
+	"MOL",
+	"MOU",
+	"MUE",
+	"ODE",
+	"OUD",
+	"ELU",
+	"LEU",
+	"LUE",
+	"LUO",
+	"OLE",
+	"123",
+	"345",
+	"456",
+	"234",
+	"135",
+	"246",
+	"111",
+	"222",
+	"333",
+	"444",
+	"555",
+	"666",
+	"MMM",
+	"OOO",
+	"UUU",
+	"DDD",
+	"LLL",
+	"EEE",
 
-	if(strcmp(scan,un)==0) 
-	{
-		return atoi(scan);
-	}
+};
 
-	if(strcmp(scan,deux)==0) 
-	{
-		return atoi(scan);
-	}
-
-	if(strcmp(scan,trois)==0) 
-	{
-		return atoi(scan);
-	}
-	else
-	{
-		printf("Impossible !");
-		get_mise();
-	}
-	return 0;
-}
-
+int tabgain[40]=
+{
+	25, //DEM
+	25, //DOM
+	250, //DEO
+	25, //DOL
+	25, //DUE
+	250, //UO
+	25, //EMU
+	1000, //LED
+	25, //LEM
+	25, //MEL
+	25, //MEO
+	250, //MOL
+	500, //MOU
+	25,  //MUE
+	25, //ODE
+	25, //OUD
+	25, //ELU
+	25, //LEU
+	25, //LUE
+	25, //LUO
+	25, //OLE
+	250, //123
+	60, //345
+	250, //456
+	40, //234
+	25, //135
+	50, //246
+	1000, //111
+	200, //222
+	300, //333
+	400, //444
+	5, //555
+	6, //666
+	100, //MMM
+	100, //OOO
+	100, //UUU
+	100, //DDD
+	100, //LLL
+	100, ///EEE
+	};
 
 int main()
 {
 	system("clear");
 	int credits = 100;
 	int mise;
-	char colone1[4];
-	char colone2[4];
+	char colone1[5];
+	char colone2[5];
 	char colone3[4];
-	char combi[12];
+	char *combi;
 	int i =0;
-	while(credits - mise > 0)
+	int caractere = 0;
+	FILE* fichier = NULL;
+	fichier = fopen("vitrine.txt", "r"); //on ouvre le fichier en lecture
+	if( fichier != NULL) //on verifie que le fichier existe bien 
+	{ 
+		while ( caractere != EOF) //
+		{   
+			caractere = fgetc(fichier);// On lit le caractère
+			if( caractere == EOF) //si c'est la fin il stop
+			{
+			   break;            
+			} 
+			printf("%c", caractere); //on print le caractere récupéré
+		}
+
+	} 	
+	fclose(fichier); 
+
+
+	while(credits != 0 && credits > 0)
 	{
+			cursorPosition(25,55);
+			printf("                  ");
+			
 			mise = get_mise();
-			srand(time(NULL));
+			cursorPosition(25,10);
+			printf("            ");
+			if(credits - mise < 0)
+			{
+				continue;
+			}
+			cursorPosition(25,55);
+			printf("               ");
+			srand(time(NULL)); 
 			int var1 = rand() % 12;
 			int var2 = rand() % 12;
-			int var3 = rand() % 12;
+			int var3 = rand() % 12; //ici on genere nos 3 variable aleatoires
 
-			strcpy(colone1, elements[var1]);
+			strcpy(colone1, elements[var1]);//avec la variable aleatoire on prend l'elements correspondant et on le copie dans une chaine
 			strcpy(colone2, elements[var2]);
-			strcpy(colone3, elements[var3]);
+			strcpy(colone3, elements[var3]);//même chose pour les 3 colonnes
 
-			printf("%s %s %s \n", colone1, colone2, colone3 );
+			combi = concatener(colone1,colone2,colone3);//on combine les 3 chaines pour en avoir une seul
 
-			strcpy(combi,colone1);
-			strcat(combi,colone2);
-			strcat(combi,colone3);
-			printf("combi : %s\n", combi);
 
-			for(i =0; i <40; i++) 
+			for(int m = 0; m < 35; m++)
 			{
-				if(strcmp(combi,combinaisons[i]) == 0)
+				int alea1 = rand() %12;
+				int alea2 = rand() %12;
+				int alea3 = rand() %12;
+				if(m<10)
 				{
-					printf("gagne\n");
-					credits = credits + tabgain[i] * mise;
-					printf("Credits : %d\n", credits);
+					affichage(alea1,2);
+
+				}
+				if(m<20)
+				{
+					affichage(alea2,43);
+					
+				}
+				if(m <25)
+				{
+					affichage(alea3,84);
+				}
+				usleep(100000);
+				affichage(var1,2);
+				affichage(var2,43);
+				affichage(var3,84);
+			}
+
+
+
+			for(i =0; i <40; i++) //on regarde dans les 40 combinaisons possibles
+			{
+				if(strcmp(combi,combinaisons[i]) == 0) //si la combinaison existe on continue
+				{
+					credits = (credits + (tabgain[i]* mise)) - mise;
+					cursorPosition(23,20);
+					printf("       ");
+					cursorPosition(23,20);
+					printf("%d\n", credits);
+					cursorPosition(23,104);
+					printf("       ");
+					cursorPosition(23,104);
+					printf("%d\n", tabgain[i] * mise);
+					cursorPosition(25,10);
+					printf("            ");
+					cursorPosition(25,10);
+					printf("VICTOIRE !");
+
 					break;
 				}
 
 			}
-			if( i == 40 && strcmp(combi,combinaisons[i]) != 0)
+			if( i == 40 && strcmp(combi,combinaisons[i]) != 0)//si la dernier est fausse le joueur a perdu
 			{
-				printf("perdu\n");
 				credits = credits - mise;
-				printf("Credits : %d\n", credits);
-			}	
+				cursorPosition(25,10);
+				printf("            ");
+				cursorPosition(25,10);
+				printf("Perdu !");
+				cursorPosition(23,20);
+				printf("       ");
+				cursorPosition(23,20);
+				printf(" %d\n", credits);
+				cursorPosition(23,104);
+				printf("       ");
+				cursorPosition(23,104);
+				printf("0");
+			}
+	}
 
-	}
-	if(credits - mise < 0)
-	{
-		printf("Credits insuffisant ! ");
-	}
 	return 0;
 }
